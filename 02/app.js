6f5b44e8f170941c7af76ecdfa545f094aedc38e
LisovAlexandr <79405023+LisovAlexandr@users.noreply.github.com>
2023-11-15 19:42:25 +0300

    test commit

--- 02/app.js
+++ 02/app.js
@@ -6,26 +6,32 @@ const listElement = document.getElementById('list')
 
 const notes=[{
         title: 'первая заметка',
-        completed: false,
+        completed: true,
     },
     {
         title: 'вторая заметка',
-        completed: true,
+        completed: false,
     }]
+
 
 //отрисовывает сохраненный список
 function render(){
-    // listElement.innerHTML 
-    // listElement.insertAdjacentHTML("beforeend",getNoteTemplate(notes[3]))
-    // listElement.insertAdjacentHTML("beforeend",getNoteTemplate(notes[0]))
-    //вариант 1
-    /*for (let i = 0; i<notes.length; i++){
-        listElement.insertAdjacentHTML("beforeend",getNoteTemplate(notes[i]))
-    }*/
-    //вариант 2
-    for (let note of notes){
-        listElement.insertAdjacentHTML("beforeend",getNoteTemplate(note.title))
+    /*listElement.innerHTML 
+    listElement.insertAdjacentHTML("beforeend",getNoteTemplate(notes[3]))
+    listElement.insertAdjacentHTML("beforeend",getNoteTemplate(notes[0]))
+    вариант 1*/
+    listElement.innerHTML=''
+    if(notes.length===0){
+        listElement.innerHTML = "<p>Нет записей</p>"
     }
+    for (let i = 0; i<notes.length; i++){
+        listElement.insertAdjacentHTML("beforeend",getNoteTemplate(notes[i],i))
+    }
+    
+    //вариант 2
+    /*for (let note of notes){
+        listElement.insertAdjacentHTML("beforeend",getNoteTemplate(note))
+    }*/
 }
 
 render()
@@ -35,27 +41,59 @@ createBtn.onclick = ()=>{
     if(inputElement.value.length === 0){
         return
     }
-    listElement.insertAdjacentHTML("beforeend",getNoteTemplate(inputElement.value))
+    
+    const newNote = {
+        title:inputElement.value, 
+        completed:false
+    }
+    notes.push(newNote)
+    render()
+    /*listElement.insertAdjacentHTML("beforeend",getNoteTemplate(newNote)),*/
     inputElement.value=""
 }
 
+listElement.onclick = function(event){
+    const ind = parseInt(event.target.dataset.index)
+    const type = event.target.dataset.type
+    //console.log(typeof ind);
+    if(type === 'toggle'){
+        //console.log(notes[ind].completed)
+        notes[ind].completed = !notes[ind].completed
+    } else if(type === 'remove'){
+        notes.splice(ind,1)
+        //console.log('нажал удалить')
+    }
+    render()
+}
+
 //функция отрисосвывает и возвращает элемент списка
-function getNoteTemplate(title){
-    return `<li
-        class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${title}</span>
-        <span>
-        <span class="btn btn-small btn-success">&check;</span>
-        <span class="btn btn-small btn-danger">&times;</span>
+function getNoteTemplate(note,index){
+    // console.log(note.title);
+    // console.log(note.completed);
+    return `
+    <li
+      class="list-group-item d-flex justify-content-between align-items-center">
+        <span class="${ note.completed ? "text-decoration-line-through" : ""}">
+            ${note.title}
         </span>
-        </li>`
+        <span>
+          <span 
+            class="btn btn-small btn-${ note.completed ? "warning" : "success" }"
+            data-type="toggle"
+            data-index="${index}"> &check; </span>
+          <span 
+            class="btn btn-small btn-danger"
+            data-type="remove"
+            data-index="${index}"> &times;</span>
+        </span>
+    </li>`
 }
 
 /*
     object theory
 */
 
-const person = {
+/*const person = {
     firstName: 'Sasha',
     lastName: 'Lisov',
     yeat: 1992,
@@ -65,4 +103,4 @@ const person = {
         return this.firstName+' '+this.lastName
     }
 }
-person.getFullName()
+person.getFullName()*/
